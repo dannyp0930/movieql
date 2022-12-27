05a770716ac46b50b158ce0f88821fb3ab4fab95
dannyp0930 <dannyp0930@gmail.com>
2022-12-27 14:46:07 +0900

    FEAT: useQuery

--- frontend/src/routes/Movies.tsx
+++ frontend/src/routes/Movies.tsx
@@ -1,29 +1,45 @@
-import { gql, useApolloClient } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
 import { Movie } from "../store/types/interface";
 
+const ALl_MOVIES = gql`
+  {
+    popularMovies {
+      title
+      id
+    }
+    topRatedMovies {
+      id
+      title
+    }
+    upcomingMovies {
+      id
+      title
+    }
+  }
+`;
+
 export default function Movies() {
-  const [popularMovies, setPopularMovies] = useState<Movie[]>([]);
-  const client = useApolloClient();
-  useEffect(() => {
-    client
-      .query({
-        query: gql`
-          {
-            popularMovies {
-              id
-              title
-            }
-          }
-        `,
-      })
-      .then((res) => setPopularMovies(res.data.popularMovies));
-  }, [client]);
+  const { data, loading, error } = useQuery(ALl_MOVIES);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>Could not fetch :(</h1>;
+  }
   return (
-    <div>
-      {popularMovies.map((movie) => (
+    <ul>
+      <h1>Popluar Movies</h1>
+      {data.popularMovies.map((movie: Movie) => (
         <li key={movie.id}>{movie.title}</li>
       ))}
-    </div>
+      <h1>Top Rated Movies</h1>
+      {data.topRatedMovies.map((movie: Movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+      <h1>Upcoming Movies</h1>
+      {data.upcomingMovies.map((movie: Movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
   );
 }
