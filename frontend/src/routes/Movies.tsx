9925478eed22068c509d41cda3bd4e67e42c821d
dannyp0930 <dannyp0930@gmail.com>
2022-12-27 14:29:21 +0900

    FEAT: ApolloProvicer

--- frontend/src/routes/Movies.tsx
+++ frontend/src/routes/Movies.tsx
@@ -1,3 +1,29 @@
+import { gql, useApolloClient } from "@apollo/client";
+import { useEffect, useState } from "react";
+import { Movie } from "../store/type/interface";
+
 export default function Movies() {
-  return <div>Movies</div>;
+  const [popularMovies, setPopularMovies] = useState<Movie[]>([]);
+  const client = useApolloClient();
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            popularMovies {
+              id
+              title
+            }
+          }
+        `,
+      })
+      .then((res) => setPopularMovies(res.data.popularMovies));
+  }, [client]);
+  return (
+    <div>
+      {popularMovies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </div>
+  );
 }
